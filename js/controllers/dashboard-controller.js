3cce5c7d1888a943c4de05f6d08c4a34c25956b0
Mani <im.manikanta@gmail.com>
2016-02-26 17:25:35 +0530

    using open weather api

--- js/controllers/dashboard-controller.js
+++ js/controllers/dashboard-controller.js
@@ -1,14 +1,16 @@
 app.components
     .controller('DashboardCtrl', DashboardController);
 
-DashboardController.inject = ['$scope', '$state'];
+DashboardController.inject = ['$scope', '$state', '$http'];
 
-function DashboardController($scope, $state) {
+function DashboardController($scope, $state, $http) {
     $scope.lat = "0";
     $scope.lng = "0";
     $scope.accuracy = "0";
     $scope.error = "";
     $scope.model = { myMap: undefined };
+    $scope.myMarkers = [];
+    $scope.weatherData = null;
 
     $scope.showError = function(error) {
         switch (error.code) {
@@ -50,6 +52,25 @@ function DashboardController($scope, $state) {
 
         var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
         $scope.model.myMap.setCenter(latlng);
+        $scope.myMarkers.push(
+            new google.maps.Marker({ map: $scope.model.myMap, position: latlng })
+        );
+
+        var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=aa03c952ea4dba3db6561d5fc09a4052' + '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+
+        $http.get(weatherUrl)
+            .then(function(response) {
+                if(response.status == 200){
+                    $scope.weatherData = {
+                        humidity: response.data.main.humidity,
+                        pressure: response.data.main.pressure,
+                        temp: response.data.main.temp - 273.15,
+                        icon: 'http://openweathermap.org/img/w/'+response.data.weather[0].icon+'.png'
+                    }
+                }
+            }, function(error) {
+                console.log(error);
+            })
     }
 
     $scope.getLocation = function() {
